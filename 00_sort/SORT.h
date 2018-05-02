dbed07f4e65a386dd128e42aa307fb1340e8f51c
simon <monkey1510@gmail.com>
2018-05-02 17:49:28 +0800

    add bubble, selection, insertion sort

---
+++ 00_sort/SORT.h
@@ -0,0 +1,29 @@
+//
+// Created by hw on 18-5-2.
+//
+
+#ifndef INC_00_SORT_SORT_H
+#define INC_00_SORT_SORT_H
+
+#include <iostream>
+
+using namespace std;
+
+class SORT {
+public:
+    void bubble();
+
+    void selection();
+
+    void insertion();
+
+    void shell();
+
+private:
+    int a[10] = {5, 2, 3, 9, 8, 1, 4, 7, 6, 0};
+
+
+};
+
+
+#endif //INC_00_SORT_SORT_H
---
+++ 00_sort/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "SORT.h"
+
+using namespace std;
+
+int main() {
+    SORT sort;
+
+    sort.insertion();
+
+    return 0;
+}
